6caeccc34af9253c95878b141c68604ae93d9bb2
darshankalathiya9 <darshankalathiya963@gmail.com>
2023-03-26 17:55:23 +0530

    Student Information Done

---
+++ Student_Information/src/main/java/Connection/DBConnection.java
@@ -0,0 +1,17 @@
+package Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+	public static Connection createconnection() {
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/student_information","root","");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+}
